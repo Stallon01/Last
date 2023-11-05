08e93fabada1f50488b8b346ed1f7840a28587f5
Stallon01 <stallonchandru@gmail.com>
2023-11-05 17:10:46 +0530

    initail dev commit

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -4,6 +4,6 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("First line");
 	System.out.println("Second line");
-	double a=5;
+	System.out.println("Dev commit");
 }
 }
